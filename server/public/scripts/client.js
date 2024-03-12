5b9d44b592a4670f1db8a0006e62376356cdd33b
Michael Kenyon <michaelkenyon2022@gmail.com>
2024-03-12 16:07:12 -0500

    Added alert if equals is clicked without all inputs entered

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,37 +12,37 @@ function getMath() {
 
     //GET request to pull calculations from server and push to DOM
     axios.get('/calculations').then((response) => {
+
         //pointing to ids in DOM where calculations will go
-        let calculationList = document.querySelector('#historyResult');
-        let calculationListRecent = document.querySelector('#resultRecent');
+        let calcList = document.querySelector('#historyResult');
+        let calcListRecent = document.querySelector('#resultRecent');
         
         //giving our returned data a var name
-        let calculationsClient = response.data;
-        console.log(calculationsClient);
+        let calcClient = response.data;
+        console.log(calcClient);
 
         //resetting calculation history section before each GET request so nothing repeats
-        calculationList.innerHTML=`
+        calcList.innerHTML=`
             <p id="history">Calculation History</p>
         `;
 
         //looping through array to display history of calculations
-        for (calculation of calculationsClient){
-        calculationList.innerHTML +=`
-        <p>${calculation.numOne} ${calculation.operator}
-            ${calculation.numTwo} = ${calculation.result}</p>
-        `
-        };
+        for (calc of calcClient){
+        calcList.innerHTML +=`
+        <p>${calc.numOne} ${calc.operator} ${calc.numTwo} = ${calc.result}</p>
+        `};
 
         //recent calculation will only populate if something is in GET request
-        if (calculationsClient.length > 0) {
-            let calculationsClientLast = calculationsClient[calculationsClient.length-1]; 
+        if (calcClient.length > 0) {
+            //creating a variable for last item in array
+            let calcClientLast = calcClient[calcClient.length-1]; 
             //console.log(calculationsClientLast);
 
-            calculationListRecent.innerHTML =`
-            <p id="recentCalc">${calculationsClientLast.numOne} ${calculationsClientLast.operator}
-            ${calculationsClientLast.numTwo} = ${calculationsClientLast.result}</P>
-        `;
-        };
+            //pushing most recent calculation to DOM
+            calcListRecent.innerHTML =`
+            <p id="recentCalc">${calcClientLast.numOne} ${calcClientLast.operator}
+            ${calcClientLast.numTwo} = ${calcClientLast.result}</P>
+        `};
 
     }).catch((error) => {
         //in case of error
@@ -52,34 +52,41 @@ function getMath() {
 
 //function for POST request on 'equal' button click
 function equals(event) {
+
     //preventing reload
     event.preventDefault();
 
      //assigning user inputs to variables
      let firstNumberInput = document.getElementById('numberFirst').value;
      let secondNumberInput = document.getElementById('numberSecond').value;
+     //console.log(secondNumberInput);
 
      //creating array from inputs and global operator
      let calculation = {
          numOne: firstNumberInput,
          numTwo: secondNumberInput,
          operator: operatorGlobal,
-         //result: "" //empty to add result later server-side
+         //result: "" //add result later server-side
      };
      console.log(calculation);
 
-    //POST request to take calculation and send to server
-    axios.post('/calculations', calculation).then((response) => {
-        //console.log response status to complete loop
-        console.log(response);
+     //if statement to prevent POST request unless all inputs have value
+     if(calculation.numOne != '' && calculation.numTwo != '' && calculation.operator != ''){
+        //POST request to take calculation and send to server
+        axios.post('/calculations', calculation).then((response) => {
+            //console.log response status to complete loop
+            console.log(response);
 
-    }).catch((error) => {
-        //in case of error
-        console.log(error);
-    });
+        }).catch((error) => {
+            //in case of error
+            console.log(error);
+        });
 
-    //calling function with GET request after completed POST request
-    getMath()
+        //calling function with GET request after completed POST request
+        getMath()
+    } else {
+        alert("Please enter all inputs and choose an operator!");
+    }
 };
 
 //Set of functions to change the global operator depending on button clicked.
